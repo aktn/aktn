d81cc1852dff2a1163ea1789ffeb7e669e4f8719
Aung Khant <theaungkhant@gmail.com>
2019-06-28 23:09:16 +0630

    Structuring index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,23 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ak`,
+    description: `Portfolio`,
+    author: `aktn`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -26,6 +37,26 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/pages/home.js
@@ -0,0 +1,92 @@
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  margin-top: 250px;
+`
+const Name = styled.p`
+  font-size: 8em;
+  position: absolute;
+  top: 188px;
+  padding-right: 400px;
+  z-index: 2;
+`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+const About = styled.div`
+  flex: 1;
+  min-width: 400px;
+  max-width: 500px;
+  min-height: 435px;
+  max-height: 600px;
+  height: 100%;
+  width: 100%;
+  background-color: cyan;
+  padding: 15px;
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Segoe UI,
+    Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+  font-size: 17px;
+  line-height: 30px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 600;
+  color: #272727;
+  background: #94d1d9;
+`
+const Picture = styled(Img)`
+  flex: 1;
+  min-width: 400px;
+  min-height: 400px;
+  height: 100%;
+  width: 100%;
+  background-color: tomato;
+  overflow: hidden;
+`
+const home = ({ data }) => {
+  const { me, description } = data.contentfulAboutMe
+
+  return (
+    <Container>
+      <Name>Aung.</Name>
+      <Wrapper>
+        <Picture fluid={me.fluid} alt={"me"} />
+        <About
+          dangerouslySetInnerHTML={{
+            __html: description.childMarkdownRemark.html,
+          }}
+        />
+      </Wrapper>
+    </Container>
+  )
+}
+
+export const query = graphql`
+  query {
+    contentfulAboutMe {
+      description {
+        description
+        childMarkdownRemark {
+          excerpt
+          html
+        }
+      }
+      me {
+        fluid(maxWidth: 1800) {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`
+
+export default home
